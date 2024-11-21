cb6906eddb8daec364d0cdd7c25b5acd589acab8
Charalampos Fanoulis <git@fanoulis.dev>
2024-11-21 05:19:19 +0200

    feat: add part 1 of 1st assignment

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include "vendor/stack.h"
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+void part_1();
+
+void main() {
+    part_1();
+}
+
+void part_1() {
+    stack_element_type input = -999;
+
+    stack_type values;
+    stack_type largest_values;
+
+    initialize(&values);
+    initialize(&largest_values);
+
+    while (input != -1) {
+        printf("Give me a number:");
+        scanf("%d", &input);
+
+        stack_element_type tmp = -1;
+
+        switch (input) {
+            case 0:
+                pop(&values, &tmp);
+            printf("Last number was %d\n", tmp);
+            break;
+
+            case -1:
+                pop(&largest_values, &tmp);
+            printf("Largest number was %d\n", tmp);
+            break;
+
+            default:
+                push(&values, input);
+
+            if(is_empty(largest_values)) {push(&largest_values, input);} else {
+                top(largest_values, &tmp);
+                if(tmp < input) push(&largest_values, input);
+            }
+
+            printf("Number stacked successfully!\n");
+            break;
+        }
+    }
 }
