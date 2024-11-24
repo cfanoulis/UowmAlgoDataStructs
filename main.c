6c87baebe9af47c87493f801c2ae4462127ead80
Charalampos Fanoulis <git@fanoulis.dev>
2024-11-24 18:57:01 +0200

    feat: 2nd part of assignment

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include "vendor/stack.h"
-#include "vendor/str_stack.h"
 
-void part_1();
-void part_2();
+int part_1();
+int part_2();
+void display_word(stack_type *stack);
 
-void main() {
-    part_1();
+
+int main() {
+    printf("Select part to run:\n1) Largest in the history of the stack\n2) Reversing sentences\n>");
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            return part_1();
+        case 2:
+            return part_2();
+        default:
+            printf("Invalid!");
+        return -1;
+    }
 }
 
-void part_1() {
+int part_1() {
     stack_element_type input = -999;
 
     stack_type values;
@@ -47,7 +59,63 @@ void part_1() {
             break;
         }
     }
-}
-void part_2() {
 
+    return 0;
+}
+
+
+int part_2() {
+    printf("Input file (escape properly!):");
+    char* filename = "";
+    scanf("%.256s", filename);
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return -1;
+    }
+
+    stack_type stack1;
+    stack_type stack2;
+
+    initialize(&stack1);
+    initialize(&stack2);
+
+    while (!feof(fp)) {
+        char in = fgetc(fp);
+
+        if(in == '.' || in == '!' || in =='?') {
+            while(!is_empty(stack1)) {
+                int tmp;
+                pop(&stack1, &tmp);
+
+                if(tmp == ' ') {
+                    display_word(&stack2);
+                    printf(" ");
+                } else {
+                    push(&stack2, tmp);
+                }
+            }
+
+            // empty stack2 for your last word
+            display_word(&stack2);
+            printf("\n");
+        } else {
+            push(&stack1, in);
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+/// Εμφανίζει τα περιεχόμενα μίας στοίβας ως μια συμβολοσειρά
+/// Η συμβολοσειρά ακολουθεί τη λογική FIFO - δηλαδή
+/// @param stack Στοίχα τύπου int που περιέχει έγκυρους κωδικούς ASCII
+void display_word(stack_type *stack) {
+    while(!is_empty(*stack)) {
+        int tmp;
+        pop(stack, &tmp);
+        printf("%c", tmp);
+    }
 }
